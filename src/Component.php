aa51f12fb4cfd967e2cc551763f021e0eaf41393
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:14:13 +0800

    Initialize Component manually

---
+++ src/Component.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\TaxonomyMetaWP;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    // const VERSION = '0.1.0';
+
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoP\TaxonomyMeta\Component::class,
+            \PoP\TaxonomiesWP\Component::class,
+        ];
+    }
+
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-taxonomymeta-wp',
+        ];
+    }
+}
